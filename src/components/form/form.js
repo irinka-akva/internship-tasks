adf39af7068f05b0d5c50bc63e958cf4a5cd319b
Irina <irinka-akva@ya.ru>
2023-10-25 10:12:06 +0300

    Добавляет верстку инпутов

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,13 +1,44 @@
 import Component from '../../app/js/base/Component';
 
 class Form extends Component {
+    form;
+    submitButton;
     constructor(element) {
         super(element);
 
-        // Your code here
+        this.form = this.getElement("inner");
+        this.submitButton = this.getElement("button-submit");
+
+        this.form.addEventListener("submit", this.handlingSubmitEvent)
     }
 
-    // Your code here
+    onSuccess = () => {
+        console.log(this.submitButton)
+        this.submitButton.disabled = true;
+    };
+
+    sendData = (formData, onSuccess) => {
+        fetch("/form", {
+            method: "POST",
+            headers: { "Content-Type": "application/json;charset=utf-8" },
+            body: formData,
+        })
+            .then((response) => {
+                if(response.ok) {
+                    onSuccess();
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+    };
+
+    handlingSubmitEvent = (e) => {
+        e.preventDefault();
+        let formData = new FormData(this.form);
+        this.sendData(formData, this.onSuccess);
+    };
 }
 
 export default Form
